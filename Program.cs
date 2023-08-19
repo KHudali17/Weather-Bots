40288bbfa7fda6b2b6f850bddf77a24f119e352c
Karam Hudali <>
2023-08-19 15:17:38 +0300

    Added a class to handle prompting the user. Program now works as expected.

--- Program.cs
+++ Program.cs
@@ -1,27 +1,27 @@
 ﻿using System.Runtime.InteropServices;
 using WeatherBots.DataAccess;
 using WeatherBots.ObserversAndPublishers;
-
+using WeatherBots.DataRecords;
 namespace WeatherBots
 {
     public class Program
     {
+        private const string FilePathPromptMessage = "Enter filepath to weather data: ";
+        private const string ConfigFileName = "Configs.json";
         static async Task Main(string[] args)
         {
-            string configFileName = "testConfigs.json";
-            DataRecords.BotConfig config = await ConfigRetrieverJson.GetConfigFromJson(configFileName);
+            var config = await ConfigRetrieverJson.GetConfigFromJson(ConfigFileName);
 
             var weatherDataPublisher = new Publisher<WeatherData>();
 
             var weatherBotService = new DynamicWeatherBotService(config, weatherDataPublisher);
 
-            WeatherDataRetrieverFactory<string> retrieverFactory = new WeatherDataRetrieverFactory<string>();
+            var retrieverFactory = new WeatherDataRetrieverFactory<string>();
 
-            //prompt for weatherdata
-            string pathToWeatherData = "";
+            var pathToWeatherData = await Prompt.PromptFilePathAsync(FilePathPromptMessage);
             var retriever = retrieverFactory.GetWeatherDataRetriever(pathToWeatherData);
 
-            WeatherData inputData = await retriever.GetWeatherData();
+            var inputData = await retriever.GetWeatherData();
 
             weatherDataPublisher.NotifyObservers(inputData);
         }
